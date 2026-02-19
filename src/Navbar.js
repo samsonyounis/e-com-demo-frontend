71ee37738c0cb3f83d125969f465a225cf0c346c
samson <samsonyounis26@gmail.com>
2026-02-19 11:46:29 +0300

    message 1

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,6 +12,9 @@ function NavBar(){
             <li>
                 <Link to="/login">Login</Link>
             </li>
+            <li>
+                <Link to="/login">About</Link>
+            </li>
         </ul>
         <Outlet></Outlet>
         </>
